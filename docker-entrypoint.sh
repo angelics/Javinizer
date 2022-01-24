0ca0e55f7945ecc98479d154f3c41dab3f4fd706
angelics <angelics@users.noreply.github.com>
2022-01-24 13:10:56 +0800

    Update docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -6,7 +6,9 @@ if [ -f /config/jvSettings.json ]; then
   echo "Using existing jvSettings config file."
 else
   echo "Copying default jvSettings file."
-  cp /root/.local/share/powershell/Modules/Javinizer/2.5.10/jvSettings.json /config/jvSettings.json
+  mkdir -p /config && pwsh -c "mv (Join-Path (Get-InstalledModule Javinizer).InstalledLocation -ChildPath jvSettings.json) /config/jvSettings.json"
+  echo "[info] creating softlink from default config file to /config"
+  pwsh -c "ln -s /config/jvSettings.json (Join-Path (Get-InstalledModule Javinizer).InstalledLocation -ChildPath jvSettings.json)"
 fi
 
 # Add Persistent jvUncensor Configuration Capability
@@ -14,7 +16,9 @@ if [ -f /config/jvUncensor.csv ]; then
   echo "Using existing jvUncensor config file."
 else
   echo "Copying default jvUncensor file."
-  cp /root/.local/share/powershell/Modules/Javinizer/2.5.10/jvUncensor.csv /config/jvUncensor.csv
+  mkdir -p /config && pwsh -c "mv (Join-Path (Get-InstalledModule Javinizer).InstalledLocation -ChildPath jvUncensor.csv) /config/jvUncensor.csv"
+  echo "[info] creating softlink from default config file to /config"
+  pwsh -c "ln -s /config/jvUncensor.csv (Join-Path (Get-InstalledModule Javinizer).InstalledLocation -ChildPath jvUncensor.csv)"
 fi
 
 # Add Persistent jvHistory Configuration Capability
@@ -22,7 +26,9 @@ if [ -f /logs/jvHistory.csv ]; then
   echo "Using existing jvHistory config file."
 else
   echo "Copying default jvHistory file."
-  cp /root/.local/share/powershell/Modules/Javinizer/2.5.10/jvHistory.csv /logs/jvHistory.csv
+  mkdir -p /logs && pwsh -c "mv (Join-Path (Get-InstalledModule Javinizer).InstalledLocation -ChildPath jvHistory.csv) /logs/jvHistory.csv"
+  echo "[info] creating softlink from default logs file to /logs"
+  pwsh -c "ln -s /logs/jvHistory.csv (Join-Path (Get-InstalledModule Javinizer).InstalledLocation -ChildPath jvHistory.csv)"
 fi
 
 # Add Persistent jvLog Configuration Capability
@@ -30,7 +36,9 @@ if [ -f /logs/jvLog.log ]; then
   echo "Using existing jvLog config file."
 else
   echo "Copying default jvLog file."
-  cp /root/.local/share/powershell/Modules/Javinizer/2.5.10/jvLog.log /logs/jvLog.log
+  mkdir -p /logs && pwsh -c "mv (Join-Path (Get-InstalledModule Javinizer).InstalledLocation -ChildPath jvLog.log) /logs/jvLog.log"
+  echo "[info] creating softlink from default logs file to /logs"
+  pwsh -c "ln -s /config/jvLog.log (Join-Path (Get-InstalledModule Javinizer).InstalledLocation -ChildPath jvLog.log)"
 fi
 
 # Add Persistent jvGenres Configuration Capability
@@ -38,7 +46,9 @@ if [ -f /config/jvGenres.csv ]; then
   echo "Using existing jvGenres config file."
 else
   echo "Copying default jvGenres file."
-  cp /root/.local/share/powershell/Modules/Javinizer/2.5.10/jvGenres.csv /config/jvGenres.csv
+  mkdir -p /config && pwsh -c "mv (Join-Path (Get-InstalledModule Javinizer).InstalledLocation -ChildPath jvGenres.csv) /config/jvGenres.csv"
+  echo "[info] creating softlink from default config file to /config"
+  pwsh -c "ln -s /config/jvGenres.csv (Join-Path (Get-InstalledModule Javinizer).InstalledLocation -ChildPath jvGenres.csv)"
 fi
 
 # Add Persistent jvTags Configuration Capability
@@ -46,7 +56,9 @@ if [ -f /config/jvTags.csv ]; then
   echo "Using existing jvTags config file."
 else
   echo "Copying default jvTags file."
-  cp /root/.local/share/powershell/Modules/Javinizer/2.5.10/jvTags.csv /config/jvTags.csv
+  mkdir -p /config && pwsh -c "mv (Join-Path (Get-InstalledModule Javinizer).InstalledLocation -ChildPath jvTags.csv) /config/jvTags.csv"
+  echo "[info] creating softlink from default config file to /config"
+  pwsh -c "ln -s /config/jvTags.csv (Join-Path (Get-InstalledModule Javinizer).InstalledLocation -ChildPath jvTags.csv)"
 fi
 
 # Add Persistent jvThumbs Configuration Capability
@@ -54,7 +66,9 @@ if [ -f /config/jvThumbs.csv ]; then
   echo "Using existing jvThumbs config file."
 else
   echo "Copying default jvThumbs file."
-  cp /root/.local/share/powershell/Modules/Javinizer/2.5.10/jvThumbs.csv /config/jvThumbs.csv
+  mkdir -p /config && pwsh -c "mv (Join-Path (Get-InstalledModule Javinizer).InstalledLocation -ChildPath jvThumbs.csv) /config/jvThumbs.csv"
+  echo "[info] creating softlink from default config file to /config"
+  pwsh -c "ln -s /config/jvThumbs.csv (Join-Path (Get-InstalledModule Javinizer).InstalledLocation -ChildPath jvThumbs.csv)"
 fi
 
 # Start the powershell universal server
